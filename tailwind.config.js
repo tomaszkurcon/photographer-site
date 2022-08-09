dd7d0db88917601959f628900d4907e3bcc0fa75
Tomasz Kurcoń <tomasz.kurcon@cetuspro.com>
2022-08-09 18:48:14 +0200

    Project setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor:"#b78b71"
+      },
+      fontFamily: {
+        body:["GlacialIndifference"]
+      }
+    },
+  },
+  plugins: [],
+}
