3b7c0901fa5f6b07d984b761b84f1da43f5c847a
Tomasz Kurcoń <tomasz.kurcon@cetuspro.com>
2022-08-18 21:11:16 +0200

    Added desktop menu

--- src/menu.js
+++ src/menu.js
@@ -2,7 +2,7 @@ const hamburger = document.getElementById("hamburger");
 const navigation = document.getElementById("navigation");
 const overlay = document.getElementById("overlay");
 const overlay2 = document.getElementById("overlay2");
-const menuBar=document.getElementById("menu-bar");
+
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navigation.classList.toggle("active");
